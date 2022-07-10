ab09d68be4f08702903831f32d12c7f4fa1ad375
tkdudgns95 <tkdudgns95@naver.com>
2022-07-11 00:05:02 +0900

    몽고db 연동, 회원가입 정보 입력

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const {User} = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
+
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://tkdudgns95:tk15128231@boiler-plate.jgnyvgq.mongodb.net/?retryWrites=true&w=majority', {
-  
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
@@ -14,6 +22,26 @@ app.get('/', (req, res) =>
   res.send('Hello World! 안녕하세요~~')
 )
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.post('/register', (req, res) => {
+
+// 회원 가입 할떄 필요한 정보들을 client에서 가져오면
+// 그것들을 데이터 베이스에 넣어준다.
+  
+
+
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false,err})
+    return res.status(200).json({ success: true
+      
+      
+    })
+  })
 })
+
+
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
